5cd73119440cfb4e69774fae3b321c6f374a09f9
Nadège <nadpulgarvidal@gmail.com>
2017-10-16 14:10:44 -0700

    Update var names to match format strings

--- g4emma/forms.py
+++ g4emma/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class SimulationForm(forms.Form):
     num_events = forms.IntegerField(required=True, label="Number of events")
-    proton_num = forms.IntegerField(required=True, label="Z", help_text="(Proton number)")
-    nucleon_num = forms.IntegerField(required=True, label="A", help_text="(Nucleon number)")
-    charge_state = forms.IntegerField(required=True, label="Q", help_text="(Charge state)")
-    kinetic_E = forms.DecimalField(required=True, label="E", help_text="MeV (Kinetic energy)")
+    beam_proton_num = forms.IntegerField(required=True, label="Z", help_text="(Proton number)")
+    beam_nucleon_num = forms.IntegerField(required=True, label="A", help_text="(Nucleon number)")
+    beam_charge_state = forms.IntegerField(required=True, label="Q", help_text="(Charge state)")
+    beam_kinetic_e = forms.DecimalField(required=True, label="E", help_text="MeV (Kinetic energy)")
