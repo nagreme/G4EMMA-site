23d1f78ab974ef1c298cbda1ef7807d838107cd2
Nadège <nadpulgarvidal@gmail.com>
2017-10-16 13:35:47 -0700

    Name all the format fields in the input strings

--- g4emma/infile_templates.py
+++ g4emma/infile_templates.py
@@ -7,75 +7,74 @@
 # Store template format strings used to build the input files
 #*************************************************************
 
-#TODO: name all the placeholders to be able to feed form_dict
 
-alphaSource_datfile = ("{}\t# YES/NO\n"
-                       "{}\t# alpha particle kinetic energy (MeV)\n"
-                       "{}\t# max angle alpha source (deg)")
+alphaSource_datfile = ("{alpha_source_present}\t# YES/NO\n"
+                       "{alpha_source_kinetic_e}\t# alpha particle kinetic energy (MeV)\n"
+                       "{alpha_source_max_agnle}\t# max angle alpha source (deg)")
 
-beam_datfile = ("{}\t# number of events\n"
-                "{}\t# beam Z\n"
-                "{}\t# beam A\n"
-                "{}\t# beam kinetic energy (MeV)\n"
-                "{}\t# fractional energy spread (FWHM)\n"
-                "{}\t# beam-spot diameter (mm)\n"
-                "{}\t# normalized transverse emittance (pi mm mrad)\n")
+beam_datfile = ("{num_events}\t# number of events\n"
+                "{beam_proton_num}\t# beam Z\n"
+                "{beam_nucleon_num}\t# beam A\n"
+                "{beam_charge_state}\t# beam kinetic energy (MeV)\n"
+                "{beam_e_spread}\t# fractional energy spread (FWHM)\n"
+                "{beam_diameter}\t# beam-spot diameter (mm)\n"
+                "{beam_trans_emittance}\t# normalized transverse emittance (pi mm mrad)\n")
 
-centralTrajectory_datfile = ("{}\t# Z\n"
-                             "{}\t# A\n"
-                             "{}\t# charge state Q\n"
-                             "{}\t#kinteic energy (MeV)\n")
+centralTrajectory_datfile = ("{center_traj_proton_num}\t# Z\n"
+                             "{center_traj_nucleon_num}\t# A\n"
+                             "{center_traj_charge_state}\t# charge state Q\n"
+                             "{center_traj_kinetic_e}\t#kinteic energy (MeV)\n")
 
-ionChamber_datfile = ("{}\t# IN/OUT\n"
-                      "{}\t# pressure (torr)\n"
-                      "{}\t# temperature (degrees Celsius)\n")
+ionChamber_datfile = ("{ion_chamber_inserted}\t# IN/OUT\n"
+                      "{ion_chamber_pressure}\t# pressure (torr)\n"
+                      "{ion_chamber_temp}\t# temperature (degrees Celsius)\n")
 
-mwpc_datfile = ("{}\t# IN/OUT\n"
-                      "{}\t# pressure (torr)\n"
-                      "{}\t# temperature (degrees Celsius)\n")
+mwpc_datfile = ("{mwpc_inserted}\t# IN/OUT\n"
+                "{mwpc_pressure}\t# pressure (torr)\n"
+                "{mwpc_temp}\t# temperature (degrees Celsius)\n")
 
 reaction_datfile = ("# Two-body reaction: 1 + 2 --> 3 + 4"
-                    "{}\t# Z1 beam\n"
-                    "{}\t# A1\n"
-                    "{}\t# Z2 beam\n"
-                    "{}\t# A2\n"
-                    "{}\t# Z3 recoil\n"
-                    "{}\t# A3\n"
-                    "{}\t# Z4 ejectile\n"
-                    "{}\t# A4\n"
-                    "{}\t# min c.m. angle ofejectile\n"
-                    "{}\t# max c.m. angle of ejectile\n"
-                    "{}\t# charge state of recoil\n"
-                    "{}\t# excitation energy of recoil\n"
-                    "{}\t# solid-angle averaged cross section (mb/sr) inside the angular range specified above\n")
+                    "{rxn_z1_beam}\t# Z1 beam\n"
+                    "{rxn_a1}\t# A1\n"
+                    "{rxn_z2_target}\t# Z2 target\n"
+                    "{rxn_a2}\t# A2\n"
+                    "{rxn_z3_recoil}\t# Z3 recoil\n"
+                    "{rxn_a3}\t# A3\n"
+                    "{rxn_z4_ejectile}\t# Z4 ejectile\n"
+                    "{rxn_a4}\t# A4\n"
+                    "{rxn_min_angle}\t# min c.m. angle ofejectile\n"
+                    "{rxn_max_angle}\t# max c.m. angle of ejectile\n"
+                    "{rxn_recoil_charge}\t# charge state of recoil\n"
+                    "{rxn_recoil_excitation_e}\t# excitation energy of recoil\n"
+                    "{rxn_cross_sec}\t# solid-angle averaged cross section (mb/sr) inside the angular range specified above\n")
 
 slits_datfile = ("# SLITS 1 (HORIZONTAL)"
-                 "{}\t# OUT/(mm)\n"
+                 "{slit_1_inserted}\t# OUT/(mm)\n"
                  "# SLITS 2 (HORIZONTAL)"
-                 "{}\t# OUT/(mm)\n"
+                 "{slit_2_inserted}\t# OUT/(mm)\n"
                  "# SLITS 3 (RIGHT)"
-                 "{}\t# OUT/(mm)\n"
+                 "{slit_3_inserted}\t# OUT/(mm)\n"
                  "# SLITS 4 (LEFT)"
-                 "{}\t# OUT/(mm)\n")
+                 "{slit_4_inserted}\t# OUT/(mm)\n")
 
 targetDegraders_datfile = ("# TARGET"
-                           "{}\t# IN/OUT/\n"
-                           "{}\t# thickness (um)\n"
-                           "{}\t# target z position offset (cm)\n"
-                           "{}\t# density (g/cm3)\n"
-                           "{}\t# number of elements\n"
-                           "{}\n" #variable number of elements here
+                           "{target_inserted}\t# IN/OUT/\n"
+                           "{target_thickness}\t# thickness (um)\n"
+                           "{target_z_pos}\t# target z position offset (cm)\n"
+                           "{target_density}\t# density (g/cm3)\n"
+                           "{target_num_elems}\t# number of elements\n"
+                           "{target_elems}\n" #variable number of elements here
                            "# DEGRADER 1"
-                           "{}\t# IN/OUT\n"
-                           "{}\t# thickness (um)\n"
-                           "{}\t# density (g/cm3)\n"
-                           "{}\t# number of elements\n"
-                           "{}\n" #variable number of elements here
+                           "{degreder_1_inserted}\t# IN/OUT\n"
+                           "{degrader_1_thickness}\t# thickness (um)\n"
+                           "{degrader_1_density}\t# density (g/cm3)\n"
+                           "{degrader_1_num_elems}\t# number of elements\n"
+                           "{degrader_1_elems}\n" #variable number of elements here
                            "# DEGRADER 2"
-                           "{}\t# IN/OUT\n"
-                           "{}\t# thickness (um)\n"
-                           "{}\t# density (g/cm3)\n"
-                           "{}\t# number of elements\n"
-                           "{}\n") #variable number of elements here
+                           "{degrader_2_inserted}\t# IN/OUT\n"
+                           "{degrader_2_thickness}\t# thickness (um)\n"
+                           "{degrader_2_density}\t# density (g/cm3)\n"
+                           "{degrader_2_num_elems}\t# number of elements\n"
+                           "{degrader_2_elems}\n") #variable number of elements here
 
-element_format_str = "{} {} {}\t# element {elem_num}: Z, M, (g/mol), mass fraction"
+element_format_str = "{elem_proton_num} {elem_molar_mass} {elem_weight_ratio}\t# element {elem_num}: Z, M, (g/mol), mass fraction"
