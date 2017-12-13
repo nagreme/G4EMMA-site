767eabe683963e81b49a7657c7c7edf1460e2ab2
Nadège <nadpulgarvidal@gmail.com>
2017-12-13 15:08:33 -0800

    Add default to desc TextField in Document model

--- g4emma/models.py
+++ g4emma/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Document(models.Model):
     filename = models.CharField(max_length=255, default=timezone.now())
     document = models.FileField(upload_to='g4emma_admin_documents/')
-    desc = models.TextField()
+    desc = models.TextField(default='')
     upload_date = models.DateTimeField(auto_now_add=True)
     last_mod_date = models.DateTimeField(auto_now=True)
 
